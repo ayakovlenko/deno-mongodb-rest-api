e81250a1da06f426b91ee3d2042afaa7a8606766
tony <ayakovlenko@users.noreply.github.com>
2021-03-29 10:52:16 +0300

    build: remove old Mongo code

--- deps_mongo.ts
+++
@@ -1,3 +0,0 @@
-import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.22.0/mod.ts";
-
-export { Bson, MongoClient };
--- deps_oak.ts
+++
@@ -1,9 +0,0 @@
-import {
-  Application,
-  Router,
-  RouterContext,
-} from "https://deno.land/x/oak@v6.5.0/mod.ts";
-
-export { Application, Router };
-
-export type { RouterContext };
